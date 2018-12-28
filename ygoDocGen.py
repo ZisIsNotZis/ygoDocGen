8e6c71feb21bc6b5bf8daa568fd71554dbd2ada3
ZisIsNotZis <ZisIsNotZis@Gmail.com>
2018-12-28 13:52:20 +0800

    Much better fix

--- ygoDocGen.py
+++ ygoDocGen.py
@@ -1,97 +1,100 @@
 def parseC(code):
     code = [i.strip() for i in code.split('\n')]
     arg = {}
-    topArg = None
+    nArg = None
     varArg = []
     for i in code:
         if 'lua_to' in i:
             I = i
-            try:
-                id = int(i.partition('lua_to')[2].split()[1][0])
-            except ValueError:
-                id = None
+            id = i.partition('lua_to')[2].split()[1].split(')')[0]
+            try: id = int(id)
+            except ValueError: pass
             i = i.split()
             if '=' in i:
                 t = i.index('=')
                 name = i[t-1]
-                if t >= 2:
-                    type = i[t-2].rstrip('*')
-                elif i[t+1][0] == '*':
-                    type = i[t+1][2:-3]
+                if i[t+1][0] == '*':
+                    Type = i[t+1][2:-3]
                 else:
-                    type = I.partition('lua_to')[2].split()[0][:-3]
+                    Type = I.partition('lua_to')[2].partition(' ')[0][:-3]
             else:
                 t = [j for j in range(len(i)) if 'lua_to' in i[j]][0]
-                if t > 0 and i[t-1][0] == '*':
-                    type = i[t-1][2:-3]
-                else:
-                    type = I.partition('lua_to')[0][:-2]
+                name = None
+                Type = I.partition('lua_to')[2].split()[0][:-3]
         elif 'check_param' in i:
             if 'check_param_count' in i:
-                id = int(i[21])
-                type = None
-                name = None
+                nArg = int(i[21:-2])
+                continue
             else:
                 i = i.partition('check_param')[2].split()
-                try:
-                    id = int(i[2][0])
-                except ValueError:
-                    id = None
-                type = i[1][11:-1].lower()
+                id = i[2].partition(')')[0].split(',')[0]
+                try: id = int(id)
+                except ValueError: pass
+                Type = i[1][11:-1].lower()
                 name = None
         elif 'lua_is' in i:
-            try:
-                i = i.partition('lua_is')[2].split()
-                id = int(i[1][0])
-            except ValueError:
-                id = None
-            type = i[0][6:-3]
+            i = i.partition('lua_is')[2].split()
+            id = i[1].partition(')')[0]
+            try: id = int(id)
+            except ValueError: pass
+            Type = i[0][:-3]
             name = None
-        elif 'lua_gettop' in i:
+        elif 'get_function_handle' in i:
+            id = int(i.partition('get_function_handle')[2].split(')')[0][4:])
+            Type = 'function'
             i = i.split()
             if '=' in i:
-                topArg = i[i.index('=')-1]
+                name = i[i.index('=')-1]
             else:
-                topArg = None
-            continue
+                name = None
         elif '2value' in i:
             id = 0
-            type = i[13:i.find('2value')]
+            Type = i[13:i.find('2value')]
             name = None
         elif 'lua_push' in i:
             id = 0
             i = i[i.find('lua_push'):].split('(')
-            type = i[0][8:]
+            Type = i[0][8:]
             name = None
         elif 'lua_yield' in i:
             id = 0
-            type = None
+            Type = None
             name = None
         else:
             continue
-        if id is None:
-            varArg.append((type, name))
+        if type(id) is str:
+            varArg.append((Type, name))
         elif id in arg:
-            arg[id].append((type, name))
+            arg[id].append((Type, name))
         else:
-            arg[id] = [(type, name)]
-    if topArg:
-        arg[max(arg, default=1)].append(('uint32', topArg))
-    return arg, varArg
+            arg[id] = [(Type, name)]
+    if nArg and not varArg:
+        arg[nArg].append((None, None))
+    return arg, varArg, nArg
 
-def fmtArg(arg, name=True):
-    if not arg: return '??? ???'
-    a = [i for i in set(i[0] for i in arg) if i]
-    b = [i for i in set(i[1] for i in arg) if i]
-    return ('|'.join(a) if len(a)>0 else '???') + (' ' + ('|'.join(b) if len(b)>0 else '???') if name else '')
+trans = dict(integer='int',
+             boolean='bool',
+             function='fn',
+             value='int')
+
+def fmtArg(arg, ret=False, nil='?'):
+    if arg:
+        a = [translate[i] if i in translate else i for i in set(i[0] for i in arg) if i]
+        b = [i.split('>')[-1] for i in set(i[1] for i in arg) if i]
+    else:
+        a = []
+        b = []
+    return ('|'.join(a) if len(a)>0 else 'nil' if ret else nil) + ' ' + ('' if ret else ('|'.join(b) if len(b)>0 else nil))
 
 def genDoc(group, name, parse):
-    arg, varArg = parse
-    doc  = '●' + (fmtArg(arg[0], False) if 0 in arg else 'void') + ' ' + group + '.' + name + '('
-    doc += ', '.join(fmtArg(arg[i] if i in arg else None) for i in range(1, max(arg, default=0)+1))
-    doc += ')'
+    arg, varArg, nArg = parse
+    doc = '●' + fmtArg(arg[0] if 0 in arg else None, True) + group + '.' + name + '('
+    arg = [fmtArg(arg[i] if i in arg else None) for i in range(1, max(arg, default=0)+1)]
     if varArg:
-        doc += '\n variable position arguments: ' + str(varArg)
+        arg.append(fmtArg(varArg) + '...')
+    if nArg and len(arg) > nArg:
+        arg = arg[:nArg] + ['[' + ', '.join(arg[nArg:]) + ']']
+    doc += ', '.join(arg) + ')'
     return doc
 
 from re import findall, DOTALL
